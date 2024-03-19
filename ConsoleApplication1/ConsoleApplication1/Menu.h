074f734151c7f2869009c0d30a1be088f7709971
Chris-KH <klowgamervn@gmail.com>
2024-03-19 18:53:32 +0700

    Tui chuyển menu qua function Menu nha

--- ConsoleApplication1/ConsoleApplication1/Menu.h
+++ ConsoleApplication1/ConsoleApplication1/Menu.h
@@ -2,19 +2,4 @@
 
 #include "Header.h"
 
-void menu() {
-    cout << "All the function: Press a number to choose";
-    cout << "1. Create a school year" << endl;
-    cout << "2. Create a semester" << endl;
-    cout << "3. View a list of the course studied in the semester (Student)" << endl;
-    cout << "4. View a list of classes" << endl;
-    cout << "5. View a list of students in a class (for example, 20APCS1…)" << endl;
-    cout << "6. View a list of courses" << endl;
-    cout << "7. View a list of students in a course" << endl;
-    cout << "8. Export a list of students in a course to a CSV file" << endl;
-    cout << "9. Import the scoreboard of a course" << endl;
-    cout << "10. View the scoreboard of a course" << endl;
-    cout << "11. Update a student's result" << endl;
-    cout << "12. View the scoreboard of a class" << endl;
-    cout << "13. View the total scoreboard (Student)" << endl;
-}
+void menu();
