0efc6224dfde65b6dfd9a0e01f86bcbe4691ec1a
nanh2303 <teddybearnov2016@gmail.com>
2024-03-20 16:56:04 +0700

    NamAnh made several changes to main.cpp

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,74 +1,74 @@
 #include "Header.h"
 
-int main() {
-	cout << "Welcome to course management system.\n";
+int main()
+{
+    // Load data
+    cout << "Welcome to course management system.\n";
 
-	UserNode* users = nullptr;
-	ifstream fin("DataFile/Users.txt");
-	if (fin.is_open()) {
-		importUserData(users, fin);
-		fin.close();
-	}
-	else {
-		cout << "Unable to load data user.\n";
-		return 0;
-	}
-
-	UserNode* logged_in = nullptr;
-	while (true) {
-		if (login(users, logged_in)) {
-			cout << "Logged in successfully.\n";
-			break;
-		}
-		else {
-			cout << "Login failed. Please check your username and password and try again.\n";
-		}
-	}
-
-	menuForStaff();
-	int n; 
-	cout << "Your choice: "; cin >> n;
+    UserNode *users = nullptr;
+    ifstream fin("DataFile/Users.txt");
+    if (fin.is_open())
+    {
+        importUserData(users, fin);
+        fin.close();
+    }
+    else
+    {
+        cout << "Unable to load data user.\n";
+        return 0;
+    }
+    // User log in
+    UserNode *logged_in = nullptr;
+    while (true)
+    {
+        if (login(users, logged_in))
+        {
+            cout << "Logged in successfully.\n";
+            break;
+        }
+        else
+        {
+            cout << "Login failed. Please check your username and password and try again.\n";
+        }
+    }
+    // Output Menu
+    menuForStaff();
+    int n;
+    cout << "Your choice: ";
+    cin >> n;
 
     switch (n)
     {
     case 1:
     {
-
     }
     case 2:
     {
-
     }
     case 3:
     {
-
     }
     case 4:
     {
-
     }
     case 5:
     {
-
     }
     case 6:
     {
-
     }
     case 7:
     {
-
     }
     case 8:
     {
-
     }
     case 9:
     {
-
     }
     case 10:
     {
     }
-	return 0;
+    }
+    return 0;
 }
