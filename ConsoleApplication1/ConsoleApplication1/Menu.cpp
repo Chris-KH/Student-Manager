ea4fba673a55a181e235a9e1b31eed6ea731d3b0
Chris-KH <klowgamervn@gmail.com>
2024-03-25 12:24:31 +0700

    .

--- ConsoleApplication1/ConsoleApplication1/Menu.cpp
+++ ConsoleApplication1/ConsoleApplication1/Menu.cpp
@@ -11,7 +11,7 @@ bool continueProgram(bool &logout)
         logoutProgram(logout);
         return false;
     }
-    cout << "Please read the instruction again!" << endl;
+    cout << "Please read the instruction again!\n";
     return continueProgram(logout);
 }
 
@@ -24,67 +24,42 @@ void logoutProgram(bool &logout)
         logout = true;
     else if (logoutChoice == 1)
         logout = false;
-    cout << "Please read the instruction again!" << endl;
+    cout << "Please read the instruction again!\n";
     logoutProgram(logout);
 }
 
 void menuForStaff()
 {
-    cout << "All the functions for staff: Press a number to choose" << endl;
-    cout << endl;
-
-    cout << "At the beginning of the year:" << endl;
-    cout << "1. Create a school year" << endl;
-    cout << "2. Create classes" << endl;
-    cout << "3. Add new 1st year students" << endl;
-    cout << endl;
-
-    cout << "At the beginning of a semester: " << endl;
-    cout << "4. Create a semester" << endl;
-    cout << "5. Add a course" << endl;
-    cout << "6. View the list of courses" << endl;
-    cout << "7. Update course information" << endl;
-    cout << "8. Add a student to the course" << endl;
-    cout << "9. Remove a student from the course" << endl;
-    cout << "10. Delete a course" << endl;
-    cout << endl;
-
-    cout << "At anytime:" << endl;
-    cout << "11. View a list of classes" << endl;
-    cout << "12. View a list of students in a class" << endl;
-    cout << "13. View a list of courses" << endl;
-    cout << "14. View a list of students in a course" << endl;
-    cout << "15. View staff info" << endl;
-    cout << "16. Change staff's password" << endl;
-    cout << endl;
-
-    cout << "At the end of a semester:" << endl;
-    cout << "17. Export a list of students in a course" << endl;
-    cout << "18. Import the scoreboard of a course" << endl;
-    cout << "19. View the scoreboard of a course" << endl;
-    cout << "20. Update a student's result" << endl;
-    cout << "21. View the scoreboard of a class" << endl;
-    cout << endl;
-
-    cout << "Staff choice: ";
+    cout << "All the functions for staff: Press a number to choose.\n";
+    cout << "   1. Create a school year.\n";
+    cout << "   2. Create classes.\n";
+    cout << "   3. Add new 1st year students.\n";
+    cout << "   4. Create a semester.\n";
+    cout << "   5. Add a course.\n";
+    cout << "   6. View the list of courses.\n";
+    cout << "   7. Update course information.\n";
+    cout << "   8. Add a student to the course.\n";
+    cout << "   9. Remove a student from the course.\n";
+    cout << "   10. Delete a course.\n";
+    cout << "   11. View a list of classes.\n";
+    cout << "   12. View a list of students in a class.\n";
+    cout << "   13. View a list of courses.\n";
+    cout << "   14. View a list of students in a course.\n";
+    cout << "   15. View staff info.\n";
+    cout << "   16. Change staff's password.\n";
+    cout << "   17. Export a list of students in a course.\n";
+    cout << "   18. Import the scoreboard of a course.\n";
+    cout << "   19. View the scoreboard of a course.\n";
+    cout << "   20. Update a student's result.\n";
+    cout << "   21. View the scoreboard of a class.\n";
+    cout << ">>>>Staff choice: ";
 }
 void menuForStudent()
 {
-    cout << "All the functions for student: Press a number to choose" << endl;
-    cout << endl;
-
-    cout << "In a semester: " << endl;
-    cout << "1. View a list of my course" << endl;
-    cout << endl;
-
-    cout << "When the scoreboard has been published by academic staff member: " << endl;
-    cout << "2. View my scoreboard" << endl;
-    cout << endl;
-
-    cout << "At any time:" << endl;
-    cout << "3. View student info" << endl;
-    cout << "4. Change student's password" << endl;
-    cout << endl;
-
-    cout << "Student choice: ";
+    cout << "All the functions for student: Press a number to choose.\n";
+    cout << "   1. View a list of my course.\n";
+    cout << "   2. View my scoreboard.\n";
+    cout << "   3. View student info.\n";
+    cout << "   4. Change student's password.\n";
+    cout << ">>>>Student choice: ";
 }
