ff2c096c3d7e0307d7039d129edb9dc190660619
Chris-KH <klowgamervn@gmail.com>
2024-03-25 11:01:33 +0700

    .

--- ConsoleApplication1/ConsoleApplication1/Year.h
+++ ConsoleApplication1/ConsoleApplication1/Year.h
@@ -16,13 +16,10 @@ struct YearNode
 	}
 };
 
-<<<<<<< HEAD
-void createNewSchoolYear(YearNode*& Head, int& NumOfSchoolYear);
-=======
+
 void createASchoolYear(YearNode*& Head, int& NumOfSchoolYear);
 
 //void importSchoolYear(YearNode *&Head, ifstream &fin, int &numOfSchoolYear);
 
->>>>>>> cb893d0b9afa11c19c79bf9f0a7b15fa9633cada
 //void exportSchoolYear(YearNode *Head, ofstream &fout, int numOfSchoolYear);
 //YearNode findSchoolYear(YearNode *yearHead, int data);
