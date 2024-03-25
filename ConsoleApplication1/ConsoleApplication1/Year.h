06a229abc065974178ab9da934c535505cfe7768
Chris-KH <klowgamervn@gmail.com>
2024-03-25 10:21:50 +0700

    .

--- ConsoleApplication1/ConsoleApplication1/Year.h
+++ ConsoleApplication1/ConsoleApplication1/Year.h
@@ -5,12 +5,12 @@
 
 struct YearNode
 {
-	int data;
+	string data;
 	SemesterInfo* semester;
 
 	YearNode* pNext;
 	YearNode() {
-		data = 0;
+		data = "";
 		pNext = nullptr;
 		semester = new SemesterInfo[3];
 	}
@@ -19,6 +19,7 @@ struct YearNode
 void createASchoolYear(YearNode*& Head, int& NumOfSchoolYear);
 
 //void importSchoolYear(YearNode *&Head, ifstream &fin, int &numOfSchoolYear);
+
 //void exportSchoolYear(YearNode *Head, ofstream &fout, int numOfSchoolYear);
 
 //YearNode findSchoolYear(YearNode *yearHead, int data);
